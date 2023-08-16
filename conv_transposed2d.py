848bb3a19f83ae0b04285f9ac4d59feb3fdd68e8
NaturalStupldity <olkhovatyi@knu.ua>
2023-08-16 20:42:08 +0300

    updated convtranspose

--- conv_transposed2d.py
+++ conv_transposed2d.py
@@ -72,36 +72,34 @@ class CustomConvTransposed2d(torch.nn.Module):
         for b in range(batch_size):
             for c_out in range(out_channels):
                 for h_out in range(out_height):
+                    h_start_out = h_out * self.stride
                     for w_out in range(out_width):
-                        h_start_out = h_out * self.stride
                         w_start_out = w_out * self.stride
-
-                        # Calculate input slice using dilation
-                        h_in = h_start_out - self.dilation * (kernel_height - 1)
-                        w_in = w_start_out - self.dilation * (kernel_width - 1)
-
-                        # Initialize output value for the current position
-                        out_value = 0.0
-
-                        # Iterate over the kernel and perform the convolution
-                        for c_in in range(in_channels):
-                            for k_row in range(kernel_height):
-                                for k_col in range(kernel_width):
-                                    h_in_current = h_in + k_row * self.dilation
-                                    w_in_current = w_in + k_col * self.dilation
-
-                                    # Check if the current indices are within valid range
-                                    if 0 <= h_in_current < in_height and 0 <= w_in_current < in_width:
-                                        out_value += padded_input[b, c_in, h_in_current, w_in_current] * self.weight[
-                                            c_in, c_out, k_row, k_col]
-
-                        output[b, c_out, h_out, w_out] = out_value
+                        output[b, c_out, h_out, w_out] = self._forward_conv(h_start_out, w_start_out, c_out, b,
+                                                                            padded_input, in_channels, in_height,
+                                                                            in_width, kernel_height, kernel_width)
         output /= batch_size
 
         if self.bias is not None:
-            output += self.bias.view(1, -1, 1, 1)  # Add bias
+            output += self.bias.view(1, -1, 1, 1)
 
         return output
+
+    def _forward_conv(self, h_start_out, w_start_out, c_out, b, padded_input, in_channels, in_height, in_width,
+                      kernel_height, kernel_width):
+        out_value = 0.0
+        h_in = h_start_out - self.dilation * (kernel_height - 1)
+        w_in = w_start_out - self.dilation * (kernel_width - 1)
+        for c_in in range(in_channels):
+            for k_row in range(kernel_height):
+                for k_col in range(kernel_width):
+                    h_in_current = h_in + k_row * self.dilation
+                    w_in_current = w_in + k_col * self.dilation
+                    if 0 <= h_in_current < in_height and 0 <= w_in_current < in_width:
+                        out_value += padded_input[b, c_in, h_in_current, w_in_current] * self.weight[
+                            c_in, c_out, k_row, k_col]
+
+        return out_value
 
     def backward(self, grad_output: torch.Tensor):
         """
@@ -111,36 +109,23 @@ class CustomConvTransposed2d(torch.nn.Module):
         :return: gradient of the loss with respect to the input of the layer
         """
 
-        assert len(grad_output.shape) == 4, "Grad output must have shape (batch_size, out_channels, out_height, out_width)"
+        assert len(grad_output.shape) == 4,\
+            "Grad output must have shape (batch_size, out_channels, out_height, out_width)"
 
         input_data, batch_size, out_channels, out_height, out_width = self.cache
+        grad_bias = torch.zeros_like(self.bias) if self.bias is not None else None
         grad_input = torch.zeros_like(input_data, device=self.weight.device)
         grad_weight = torch.zeros_like(self.weight)
-        grad_bias = torch.zeros_like(self.bias) if self.bias is not None else None
-
-        kernel_height = self.weight.size(2)
-        kernel_width = self.weight.size(3)
-        in_channels = self.weight.size(0)
-        in_height = input_data.size(2)
-        in_width = input_data.size(3)
 
         for b in range(batch_size):
             for c_out in range(out_channels):
                 for h_out in range(out_height):
+                    h_start = h_out * self.stride
                     for w_out in range(out_width):
-                        h_start = h_out * self.stride
                         w_start = w_out * self.stride
-                        for k_row in range(kernel_height):
-                            for k_col in range(kernel_width):
-                                h_in = h_start + k_row - self.padding
-                                w_in = w_start + k_col - self.padding
-                                for c_in in range(in_channels):  # Loop over input channels
-                                    if 0 <= h_in < in_height and 0 <= w_in < in_width:
-                                        grad_input[b, c_in, h_in, w_in] += \
-                                            self.weight[c_in, c_out, k_row, k_col] * grad_output[b, c_out, h_out, w_out]
-                                        grad_weight[c_in, c_out, k_row, k_col] += \
-                                            input_data[b, c_in, h_in, w_in] * grad_output[b, c_out, h_out, w_out]
-
+                        grad_input, grad_weight, grad_bias = self._conv_backward(grad_output, input_data, grad_input,
+                                                                                 grad_bias, grad_weight, h_start,
+                                                                                 w_start, h_out, w_out, c_out, b)
                         if self.bias is not None:
                             grad_bias[c_out] += grad_output[b, c_out, h_out, w_out]
 
@@ -149,3 +134,21 @@ class CustomConvTransposed2d(torch.nn.Module):
             self.bias.grad = grad_bias / batch_size
 
         return grad_input, grad_weight, grad_bias
+
+    def _conv_backward(self, grad_output: torch.Tensor, input_data: torch.Tensor, grad_input: torch.Tensor,
+                       grad_bias: torch.Tensor, grad_weight: torch.Tensor, h_start: int, w_start: int, h_out: int,
+                       w_out: int, c_out: int, b: int):
+
+        for k_row in range(self.weight.size(2)):
+            h_in = h_start + k_row - self.padding
+            for k_col in range(self.weight.size(3)):
+                w_in = w_start + k_col - self.padding
+                for c_in in range(self.weight.size(0)):
+                    if 0 <= h_in < input_data.size(2) and 0 <= w_in < input_data.size(3):
+                        grad_input[b, c_in, h_in, w_in] += \
+                            self.weight[c_in, c_out, k_row, k_col] * grad_output[b, c_out, h_out, w_out]
+                        grad_weight[c_in, c_out, k_row, k_col] += \
+                            input_data[b, c_in, h_in, w_in] * grad_output[b, c_out, h_out, w_out]
+
+        return grad_input, grad_weight, grad_bias
+
